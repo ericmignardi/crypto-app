4be8085fba02c66a08115e1cbc772ad7a61da058
Eric Mignardi <mignardi.e@gmail.com>
2025-06-24 11:43:19 -0400

    Initial commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,59 @@
+import React, { useContext } from "react";
+import "./Navbar.css";
+import logo from "../../assets/logo.png";
+import arrowIcon from "../../assets/arrowIcon.png";
+import { CoinContext } from "../../contexts/CoinContext";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  const { setCurrency } = useContext(CoinContext);
+
+  const currencyHandler = (event) => {
+    switch (event.target.value) {
+      case "usd": {
+        setCurrency({ name: "usd", symbol: "$" });
+        break;
+      }
+      case "eur": {
+        setCurrency({ name: "eur", symbol: "€" });
+        break;
+      }
+      case "inr": {
+        setCurrency({ name: "inr", symbol: "₹" });
+        break;
+      }
+      default: {
+        setCurrency({ name: "usd", symbol: "$" });
+        break;
+      }
+    }
+  };
+
+  return (
+    <div className="navbar">
+      <Link to={"/"}>
+        <img className="logo" src={logo} alt="" />
+      </Link>
+      <ul>
+        <Link to={"/"}>
+          <li>Home</li>
+        </Link>
+        <li>Features</li>
+        <li>Pricing</li>
+        <li>Blog</li>
+      </ul>
+      <div className="navRight">
+        <select onChange={currencyHandler}>
+          <option value="usd">USD</option>
+          <option value="eur">EUR</option>
+          <option value="inr">INR</option>
+        </select>
+        <button>
+          Sign Up <img src={arrowIcon} alt="" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
