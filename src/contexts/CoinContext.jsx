1d4f6bba5e93c0326ae8c9ea030946f0f9a7ab3a
Eric Mignardi <mignardi.e@gmail.com>
2025-06-24 11:52:17 -0400

    Minor changes to CoinContext.jsx

--- src/contexts/CoinContext.jsx
+++ src/contexts/CoinContext.jsx
@@ -14,7 +14,7 @@ const CoinContextProvider = (props) => {
       method: "GET",
       headers: {
         accept: "application/json",
-        "x-cg-demo-api-key": `${import.meta.env.VITE_API_KEY}`,
+        "x-cg-demo-api-key": import.meta.env.VITE_API_KEY,
       },
     };
 
